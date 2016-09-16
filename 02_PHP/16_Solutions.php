aacbf37753ac63f0fb42e9f3f46e08ca33402257
Susan Buck <susan@susanbuck.net>
2016-09-15 22:50:23 -0400

    Switched up how solutions are shared

---
+++ 02_PHP/16_Solutions.php
@@ -0,0 +1,11 @@
+# Solutions
+
+
+The following three examples were covered in these notes:
++ Clock
++ Raffle v1 (with three different approaches)
++ Raffle v2 (with form input)
+
+You can see the finished code for these examples here: <https://github.com/susanBuck/dwa15-php-practice>
+
+And you can see them in action here: <http://php-practice.dwa15.com>
